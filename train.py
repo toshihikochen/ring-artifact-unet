3ad26a09b83e7d5bab5198feb30caafd0aba422e
Toshihiko <toshihikochen@qq.com>
2026-01-26 23:22:02 +0800

    add fpn residual dense unet

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import lightning as pl
 import torch
 from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
 
-from models.residual_dense_unet_gradloss import RingArtifactResidualDenseUNet
+from models.fpn_residual_dense_unet_gradloss import RingArtifactFPNResidualDenseUNet
 from data_modules.tiff_data import RingArtifactTIFFDataModule
 
 random.seed(42)
@@ -16,15 +16,15 @@ torch.set_float32_matmul_precision("high")
 
 def main():
     data_module = RingArtifactTIFFDataModule(train_dir="data/tiff/train", val_dir="data/tiff/val", batch_size=4)
-    model = RingArtifactResidualDenseUNet()
+    model = RingArtifactFPNResidualDenseUNet()
     trainer = pl.Trainer(
         accelerator="gpu",
         precision="bf16-mixed",
         accumulate_grad_batches=2,
         max_epochs=200,
         logger=[
-            CSVLogger(save_dir="lightning_logs", name="residual_denseunet_gradloss"),
-            TensorBoardLogger(save_dir="lightning_logs", name="residual_denseunet_gradloss"),
+            CSVLogger(save_dir="lightning_logs", name="fpn_residual_denseunet_gradloss"),
+            TensorBoardLogger(save_dir="lightning_logs", name="fpn_residual_denseunet_gradloss"),
         ],
         log_every_n_steps=10,
     )
